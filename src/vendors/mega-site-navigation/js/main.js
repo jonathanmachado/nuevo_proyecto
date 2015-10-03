3ecce1664efb7146ec7538cf296e2dc0fd7c5197
jonathanmachado <jonathanmachado48@gmail.com>
2015-10-03 20:52:15 -0300

    home page 70%

--- src/vendors/mega-site-navigation/js/main.js
+++ src/vendors/mega-site-navigation/js/main.js
@@ -1,6 +1,25 @@
+var MqL = 1170;
+
+function toggleSearch(type) {
+	if(type=="close") {
+		//close serach
+		$('.search').removeClass('is-visible');
+		$('.search-trigger').removeClass('search-is-visible');
+		$('.overlay').removeClass('search-is-visible');
+		$('.sticky').removeClass('search-visible');
+	} else {
+		//toggle search visibility
+		$('.search').toggleClass('is-visible');
+		$('.search-trigger').toggleClass('search-is-visible');
+		$('.overlay').toggleClass('search-is-visible');
+		$('.sticky').toggleClass('search-visible');
+		if($('.top-bar').hasClass('expanded')) $('.top-bar').removeClass('expanded');
+		if($(window).width() > MqL && $('.search').hasClass('is-visible')) $('.search').find('input[type="search"]').focus();
+		($('.search').hasClass('is-visible')) ? $('.overlay').addClass('is-visible') : $('.overlay').removeClass('is-visible') ;
+	}
+}
+
 jQuery(document).ready(function($){
-	//if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
-	var MqL = 1170;
 
 	//open search form
 	$('.search-trigger').on('click', function(event){
@@ -13,26 +32,6 @@ jQuery(document).ready(function($){
 		toggleSearch('close')
 		$('.overlay').removeClass('is-visible');
 	});
-
-
-
-	function toggleSearch(type) {
-		if(type=="close") {
-			//close serach
-			$('.search').removeClass('is-visible');
-			$('.search-trigger').removeClass('search-is-visible');
-			$('.overlay').removeClass('search-is-visible');
-			$('.sticky').removeClass('search-visible');
-		} else {
-			//toggle search visibility
-			$('.search').toggleClass('is-visible');
-			$('.search-trigger').toggleClass('search-is-visible');
-			$('.overlay').toggleClass('search-is-visible');
-			$('.sticky').toggleClass('search-visible');
-			if($(window).width() > MqL && $('.search').hasClass('is-visible')) $('.search').find('input[type="search"]').focus();
-			($('.search').hasClass('is-visible')) ? $('.overlay').addClass('is-visible') : $('.overlay').removeClass('is-visible') ;
-		}
-	}
 
 
 });
